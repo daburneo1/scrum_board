d7fc3ca2edccfe253dd33cebd16b89676792ee8a
David Burneo <davichov2013@gmail.com>
2026-08-01 21:44:26 -0500

    feat: migration to protect columns in cascade delete and migration config to access at env

---
+++ backend/wsScrumBoard/src/Infrastructure/Persistence/Migrations/20260802024335_RestrictColumnDeletionWithTasks.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestrictColumnDeletionWithTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_board_tasks_board_columns_column_id",
+                table: "board_tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_board_tasks_board_columns_column_id",
+                table: "board_tasks",
+                column: "column_id",
+                principalTable: "board_columns",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_board_tasks_board_columns_column_id",
+                table: "board_tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_board_tasks_board_columns_column_id",
+                table: "board_tasks",
+                column: "column_id",
+                principalTable: "board_columns",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
